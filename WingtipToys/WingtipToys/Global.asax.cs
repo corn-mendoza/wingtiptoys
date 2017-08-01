30f98c597d6a89d006aeda7e7a22e28d188a5c89
Corn Mendoza <cmendoza@pivotal.io>
2017-08-01 16:50:21 -0500

    Change to allow unhandled exceptions to be displayed

--- WingtipToys/WingtipToys/Global.asax.cs
+++ WingtipToys/WingtipToys/Global.asax.cs
@@ -52,18 +52,18 @@ namespace WingtipToys
         {
           // Code that runs when an unhandled error occurs.
 
-          // Get last error from the server
-          Exception exc = Server.GetLastError();
+          //// Get last error from the server
+          //Exception exc = Server.GetLastError();
 
-          if (exc is HttpUnhandledException)
-          {
-            if (exc.InnerException != null)
-            {
-              exc = new Exception(exc.InnerException.Message);
-              Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
-                  true);
-            }
-          }
+          //if (exc is HttpUnhandledException)
+          //{
+          //  if (exc.InnerException != null)
+          //  {
+          //    exc = new Exception(exc.InnerException.Message);
+          //    Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
+          //        true);
+          //  }
+          //}
         }
     }
 }
