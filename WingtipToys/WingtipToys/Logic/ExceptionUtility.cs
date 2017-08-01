6606b848324db57964878c82bbe5fcbae96f1dbb
Corn Mendoza <cmendoza@pivotal.io>
2017-08-01 16:23:39 -0500

    Change logging to console for PCF

--- WingtipToys/WingtipToys/Logic/ExceptionUtility.cs
+++ WingtipToys/WingtipToys/Logic/ExceptionUtility.cs
@@ -6,49 +6,42 @@ using System.IO;
 
 namespace WingtipToys.Logic
 {
-  // Create our own utility for exceptions
-  public sealed class ExceptionUtility
-  {
-    // All methods are static, so this can be private
-    private ExceptionUtility()
-    { }
-
-    // Log an Exception
-    public static void LogException(Exception exc, string source)
+    // Create our own utility for exceptions
+    public sealed class ExceptionUtility
     {
-      // Include logic for logging exceptions
-      // Get the absolute path to the log file
-      string logFile = "App_Data/ErrorLog.txt";
-      logFile = HttpContext.Current.Server.MapPath(logFile);
+        // All methods are static, so this can be private
+        private ExceptionUtility()
+        { }
 
-      // Open the log file for append and write the log
-      StreamWriter sw = new StreamWriter(logFile, true);
-      sw.WriteLine("********** {0} **********", DateTime.Now);
-      if (exc.InnerException != null)
-      {
-        sw.Write("Inner Exception Type: ");
-        sw.WriteLine(exc.InnerException.GetType().ToString());
-        sw.Write("Inner Exception: ");
-        sw.WriteLine(exc.InnerException.Message);
-        sw.Write("Inner Source: ");
-        sw.WriteLine(exc.InnerException.Source);
-        if (exc.InnerException.StackTrace != null)
+        // Log an Exception
+        public static void LogException(Exception exc, string source)
         {
-          sw.WriteLine("Inner Stack Trace: ");
-          sw.WriteLine(exc.InnerException.StackTrace);
+            Console.WriteLine("********** {0} **********", DateTime.Now);
+            if (exc.InnerException != null)
+            {
+                Console.Write("Inner Exception Type: ");
+                Console.WriteLine(exc.InnerException.GetType().ToString());
+                Console.Write("Inner Exception: ");
+                Console.WriteLine(exc.InnerException.Message);
+                Console.Write("Inner Source: ");
+                Console.WriteLine(exc.InnerException.Source);
+                if (exc.InnerException.StackTrace != null)
+                {
+                    Console.WriteLine("Inner Stack Trace: ");
+                    Console.WriteLine(exc.InnerException.StackTrace);
+                }
+            }
+            Console.Write("Exception Type: ");
+            Console.WriteLine(exc.GetType().ToString());
+            Console.WriteLine("Exception: " + exc.Message);
+            Console.WriteLine("Source: " + source);
+            Console.WriteLine("Stack Trace: ");
+            if (exc.StackTrace != null)
+            {
+                Console.WriteLine(exc.StackTrace);
+                Console.WriteLine();
+            }
+
         }
-      }
-      sw.Write("Exception Type: ");
-      sw.WriteLine(exc.GetType().ToString());
-      sw.WriteLine("Exception: " + exc.Message);
-      sw.WriteLine("Source: " + source);
-      sw.WriteLine("Stack Trace: ");
-      if (exc.StackTrace != null)
-      {
-        sw.WriteLine(exc.StackTrace);
-        sw.WriteLine();
-      }
-      sw.Close();
     }
-  }
 }
